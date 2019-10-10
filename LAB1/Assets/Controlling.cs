b4d9eb4a9afee2e613f8334b6ff6c6a958cb93fb
zlatayanytska <32952956+zlatayanytska@users.noreply.github.com>
2019-10-11 00:23:17 +0300

    Add files via upload

---
+++ LAB1/Assets/Controlling.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlling: MonoBehaviour {
+	public CharacterController controller;
+    public float jump;
+    public float jumpSpeed;
+    public float gravity;
+    public Vector3 Vector;
+	public float jumpBoost;
+	public float speed;
+	
+    
+	void Start() {
+		speed = 5f;
+		jump = 1f;
+        gravity = 1f;
+        controller = this.GetComponent<CharacterController>();
+
+    }
+
+    void Update() {
+        jumpBoost -= gravity * Time.deltaTime;
+			    if (Input.GetKeyDown(KeyCode.Space)) {
+                jumpBoost = jump;
+			}
+
+		Vector = new Vector3(Input.GetAxis("Horizontal"), jumpBoost, Input.GetAxis("Vertical"));
+		Vector *= speed;
+		transform.LookAt(transform.position + new Vector3(Vector.x, 0, Vector.z));
+		controller.Move(Vector * Time.deltaTime);
+	}
+}
---
+++ LAB1/Assets/View.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class View: MonoBehaviour {
+	private Vector3 cameraPos;
+	public Transform PlayerPos;
+
+	[Range(0.01f, 1.0f)]
+	public float SmoothFactor = 0.5f;
+	void Start() {
+		cameraPos = transform.position - PlayerPos.position;
+	}
+	void Update() {
+        Vector3 updatedPosition = PlayerPos.position + cameraPos;
+		transform.position = Vector3.Slerp(transform.position, updatedPosition, SmoothFactor);
+		transform.LookAt(PlayerPos.transform);
+	}
+}
